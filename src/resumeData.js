b65fd25a67acfc07c1db0dc84e0f999f374bc92e
Bruno Lima <ibrunoliimas@gmail.com>
2019-05-17 02:32:13 -0300

    CriaÃao de primeiro commit antes de alteracao

---
+++ src/resumeData.js
@@ -0,0 +1,105 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Bruno Lima",
+    "role": "Frontend Developer and Ux Designer",
+    "linkedinId":"brunoliimas",
+    "roleDescription": "Description",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://linkedin.com/in/brunoliimas",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/brunoliimas",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"Instagram",
+          "url":"http://instagram.com/brunoliimas",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"São Paulo/SP",
+    "website":"Site",
+    "education":[
+      {
+        "SchoolName":"Mastertech",
+        "specialization":"Imersivo de Programação",
+        "MonthOfPassing":"Jul",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "SchoolName":"Senac",
+        "specialization":"Técnico em Logística",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2015",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Mastertech",
+        "specialization":"Assistente Administrativo/Relacionamento B2B",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2019",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Mastertech",
+        "specialization":"Ninja",
+        "MonthOfLeaving":"Ago",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+export default resumeData
