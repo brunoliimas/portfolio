cbf17098be6a1c73df8d0e59d6f94e639d03b6aa
Bruno Lima <ibrunoliimas@gmail.com>
2019-09-19 19:25:25 -0300

    builld add

---
+++ build/precache-manifest.e98c69f128f1bc03e0e4b5e5fab46af0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9ab9023d44e5faad4defa821a71a2a",
+    "url": "/brunoliimas/portfolio/index.html"
+  },
+  {
+    "revision": "9e43fb137c86504e4d51",
+    "url": "/brunoliimas/portfolio/static/css/main.e8986148.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/brunoliimas/portfolio/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "9e43fb137c86504e4d51",
+    "url": "/brunoliimas/portfolio/static/js/main.c10e4730.chunk.js"
+  },
+  {
+    "revision": "ddc7de9cd3e5432ee391",
+    "url": "/brunoliimas/portfolio/static/js/runtime~main.af6d4eec.js"
+  }
+]);
